03107e3b349888883794bd589d4d03d969954a4a
daniel <dzielinski@9bits.pl>
2023-02-22 09:17:10 +0100

    refactor - remove @PostConstruct

--- src/main/java/com/daniel/zielinski/binanceconnector/config/BinanceProperties.java
+++ src/main/java/com/daniel/zielinski/binanceconnector/config/BinanceProperties.java
@@ -5,7 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Validated
@@ -13,5 +14,5 @@ import java.util.List;
 @ConfigurationProperties(prefix = "app.binance")
 public class BinanceProperties {
 
-    private List<String> tickers;
+    private Set<String> tickers = new HashSet<>();
 }
